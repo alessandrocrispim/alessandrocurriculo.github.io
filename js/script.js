437e512997a4a5101bc0d701b80159f3d970706d
alessandrocrispim <alessandro.crispim.1979@gmail.com>
2022-10-18 14:06:03 -0300

    Curriculo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.header-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+};
+
